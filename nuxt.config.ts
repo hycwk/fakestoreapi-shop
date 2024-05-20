e3f328b547d08ed8a9c2acf5ad717b43a2fd3ec4
Eric Wong <eric1152001@gmail.com>
2024-05-20 16:20:20 +1000

    chore: Add @nuxtjs/google-fonts module and update package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss',
+		['@nuxtjs/google-fonts', {
+			families: {
+				'Roboto+Condensed': [400, 600, 800],
+			}
+		}],
+	],
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: [],
   theme: {
     extend: {},
+		fontFamily: {
+			store: ['Roboto Condensed', 'Arial', 'sans-serif'],
+		},
   },
   plugins: [require('daisyui')],
 }
